99d520ee7149f1d144048872581599f67be52c5d
Peter Simon <Peter@Peters-Mac-mini.local>
2018-01-19 23:01:48 +0900

    added pair.txt file. print all coin/market pair

--- bot.py
+++ bot.py
@@ -3,22 +3,23 @@
 
 import requests
 import json
+import time
 
-markets = {
-    'KRW':['BTC', 'ETH', 'XRP'],
-    'BTC':['BTC', 'ETH', 'XRP'],
-    'ETH':['BTC', 'ETH', 'XRP'],
-    'USDT':['BTC', 'ETH', 'XRP']
-}
 
 if __name__ == '__main__':
-    for market in markets:
-        for coin in markets[market]:
-            url = 'https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.KRW-BTC'.format(market, coin)
-            print (url)
-            r = requests.get(url, auth=('user', 'pass'))
-            if r.status_code == 200:
-                print (r.status_text)
+    with open('pair.txt') as myfile:
+        lines = myfile.readlines()
+        for idx, line, in enumerate(lines):
+            # 0: index, -1: pair
+            cols = line.split(',')
+            pair = cols[-2]
+
+            # 0: coin, 1: market
+            pair_info= pair.split('/')
+            coin = pair_info[0]
+            market = pair_info[1]
+
 '''
-[{"code":"CRIX.UPBIT.KRW-BTC","candleDateTime":"2018-01-19T11:36:00+00:00","candleDateTimeKst":"2018-01-19T20:36:00+09:00","openingPrice":14884000.00000000,"highPrice":14890000.00000000,"lowPrice":14874000.0,"tradePrice":14874000.0,"candleAccTradeVolume":15.35527905,"candleAccTradePrice":228506807.11562000,"timestamp":1516361810391,"unit":1}]
+url = 'https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.KRW-BTC'.format(market, coin)
+[{"code":"CRIX.UPBIT.KRW-BTC","candleDateTime":"2018-01-19T11:36:00+00:00","candleDateTimeKst":"2018-01-19T20:36:00+09:00","openingPrice":14884000.00000000,"highPrice":14890000.00000000,"lowPrice":14874000.0,"tradePrice":14874000.0,"candleAccTradeVolume":15.35527905,"candleAccT
 '''
