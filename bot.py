0ebc8dc38697ceddac4b11b5058ca8a1b6ae5ab3
fisherman.moon@gmail.com <jsmoon@Macui-MacBook-Pro.local>
2018-01-21 00:17:28 +0900

    added

--- bot.py
+++ bot.py
@@ -4,9 +4,11 @@
 import requests
 import json
 import time
+import Thread
+from threading import Thread
 
 
-if __name__ == '__main__':
+def update(thread_name):
     with open('pair.txt') as myfile:
         lines = myfile.readlines()
         for idx, line, in enumerate(lines):
@@ -19,7 +21,17 @@ if __name__ == '__main__':
             coin = pair_info[0]
             market = pair_info[1]
 
+            url = 'https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.{}-{}'.format(market, coin)
+            r = requests.get(url)
+            if r.status_code == 200:
+                print (r.text)
+
+
+if __name__ == '__main__':
+    t1 = Thread( target=thread1, args=("Thread-1", ) )
+    t1.start()
+
+
 '''
-url = 'https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.KRW-BTC'.format(market, coin)
 [{"code":"CRIX.UPBIT.KRW-BTC","candleDateTime":"2018-01-19T11:36:00+00:00","candleDateTimeKst":"2018-01-19T20:36:00+09:00","openingPrice":14884000.00000000,"highPrice":14890000.00000000,"lowPrice":14874000.0,"tradePrice":14874000.0,"candleAccTradeVolume":15.35527905,"candleAccT
 '''
